056a101499797be80365cf9f2268895691f48361
Marisa <ma-ah02@live.com>
2020-10-21 11:12:40 -0300

    add FormWithXState

--- src/pages/FormWithXState.tsx
+++ src/pages/FormWithXState.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableWithoutFeedback, TextInput, Button } from 'react-native';
-import { createMachine, assign } from 'xstate';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { assign, Machine } from 'xstate';
 import { useMachine } from '@xstate/react'
-import CheckBox from "@react-native-community/checkbox";
+import { Picker } from '@react-native-community/picker';
+import { CheckBox } from "native-base"
 
-const formMachine = createMachine({
+const initialContext = {
+  name: '',
+  CPF: '',
+  birthday: '',
+  checked: false,
+  favoriteFood: 'macarrão',
+}
+
+const formMachine = Machine({
   initial: 'idle',
-  context: {
-    name: '',
-    CPF: '',
-    birthday: '',
-    checked: false,
-    favoriteBand: '',
-  },
+  context: initialContext,
   states: {
     idle: {
       entry: "log",
@@ -26,54 +29,73 @@ const formMachine = createMachine({
     submitting: {
       on: {
         stoppedTyping: {
+          actions: "log",
+        },
+        submit: {
           target: "idle",
-          actions: "log"
+          actions: "clear",
         }
-      }
+      },
     },
   },
-},
-{
-  actions: {
-    typing: assign((context, event) => {
-      console.log("evento", event)
-      return ({...context, inputValue: event.value})
-    }),
-    log: (context, e) => console.log(context.inputValue),
-  }
-},
+  },
+  {
+    actions: {
+      typing: assign((context, {name, value}) => ({...context, [name]: value})),
+      log: (context, event) => console.log(context),
+      clear: assign(() => initialContext)
+    }
+  },
 )
 
 interface IHandleChange {
   name: string
-  value: string
+  value: string | boolean | number
 }
 
 const FormWithXState = () => {
   const [current, send] = useMachine(formMachine)
 
-  useEffect(() => {
-    current
-  }, [current])
-
-  const handleChange = ({ name, value }:IHandleChange) => send("typing", { [name]: value })
+  const handleChange = ({ name, value }:IHandleChange) => send("typing", { name, value })
 
   return (
-    <View style={styles.container}>
+    <View>
+      <Text style={styles.text}>Nome </Text>
       <TextInput
+        value={current.context.name}
         style={styles.inputText}
         onChangeText={text => handleChange({ name: "name", value: text})}
       />
+      <Text style={styles.text}>CPF </Text>
       <TextInput
+        value={current.context.CPF}
         style={styles.inputText}
         onChangeText={text => handleChange({ name: "CPF", value: text})}
       />
-      <CheckBox
-        onValueChange={value => handleChange({ name: "checked", value: value})}
-      />
-      <Button color="#841584" title="Submit" onPress={() => {
-        send("submit")
+      <Text style={styles.text}>Comida favorita </Text>
+      <Picker
+        selectedValue={current.context.favoriteFood}
+        style={styles.dropdown}
+        onValueChange={(itemValue, itemIndex) =>
+          handleChange({name: "favoriteFood", value: itemValue })
+        }>
+        <Picker.Item label="Macarrão" value="Macarrão" />
+        <Picker.Item label="Lasanha" value="Lasanha" />
+        <Picker.Item label="Pizza" value="Pizza" />
+      </Picker>
+      <View style={styles.container}>
+        <CheckBox
+          checked={current.context.checked}
+          color={current.context.checked ? "#fc5185" : "#000030" }
+          onPress={() => handleChange({ name: "checked", value: !current.context.checked})}
+        />
+        <Text style={{...styles.checkBoxText, color: current.context.checked ? "#fc5185" : "#000030" }}>
+          MARCA AQUI!!
+        </Text>
+      </View>
+      <Button color="#841584" title="Enviar" onPress={() => {
         send("stoppedTyping")
+        send("submit")
       }} />
     </View>
   );
@@ -81,12 +103,35 @@ const FormWithXState = () => {
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "flex-start",
+    marginVertical: 20,
+    backgroundColor: "#F0EBE9",
+    paddingVertical: 5,
+    borderRadius: 50,
   },
   inputText: {
     height: 40,
-    width: 100,
-    borderColor: 'gray',
-    borderWidth: 1
+    width: 300,
+    borderWidth: 0,
+    borderBottomWidth: 1,
+  },
+  text: {
+    marginTop: 20,
+    fontWeight: "bold",
+    color: "#841584",
+  },
+  dropdown: {
+    width: 300,
+    height: 200,
+    marginTop: -40,
+  },
+  checkBoxText: {
+    fontWeight: "bold",
+    marginTop: 2,
+    marginLeft: 20,
   }
 });
 
