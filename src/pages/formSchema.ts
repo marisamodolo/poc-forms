346cdf9d89808e822d78ced15a5ac60707a85bee
ThaisRobba <thais.robba@gmail.com>
2020-11-05 18:04:13 -0300

    document nope-validator vs yup

--- src/pages/formSchema.ts
+++ src/pages/formSchema.ts
@@ -19,4 +19,36 @@ const FormSchema = Yup.object().shape({
     .required('É necessário confirmar a senha'),
 })
 
+/*
+import Nope from "nope-validator";
+
+const FormSchema = Nope.object().shape({
+  name: Nope.string()
+    .greaterThan(1, "Muito curto!")
+    .lessThan(50, "Muito longo!")
+    .required("É necessário informar o nome."),
+  CPF: Nope.string()
+    .min(11, "CPF incorreto.")
+    .regex(/[0-9]{11}/, "CPF inválido.")
+    .required("É necessário informar o CPF."),
+  checked: Nope.boolean()
+    .oneOf([true], "É necessário marcar.")
+    .required("É necessário marcar."),
+  password: Nope.string()
+    .greaterThan(7, "Senha muito pequena")
+    .regex(/[A-Za-z0-9_.]{11}/, "Senha inválida.")
+    .required("É necessário ter uma senha"),
+  passwordConfirmation: Nope.string()
+    .oneOf([Nope.ref("password")], "Confirmação de senha incorreta")
+    .required("É necessário confirmar sua senha")
+});
+
+
+// Com o nope, a gente não precisa criar/usar um utilitário pois por padrão ele devolve um dicionário de erros
+console.log(
+  FormSchema.validate(formData)
+);
+
+*/
+
 export default FormSchema
